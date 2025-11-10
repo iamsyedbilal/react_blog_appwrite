a901461210660e1b91b552dffc7863f6e46420d1
iamsyedbilal <syedbilal.dev27@gmail.com>
2025-11-10 19:36:31 +0500

    Fixes typo in loading slice import

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "../features/themeSlice/themeSlice";
 import authReducer from "../features/authSlice/authSlice";
-import loadingReducer from "../features/loadingSlice/LoadingSlice";
+import loadingReducer from "../features/loadingSlice/loadingSlice";
 
 export const store = configureStore({
   reducer: {
